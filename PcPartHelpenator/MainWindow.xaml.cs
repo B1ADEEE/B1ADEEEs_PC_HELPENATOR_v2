60b2e1b1a382108087a2040883cb8f3ca2ede949
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2021-03-22 18:10:25 +0000

    Add project files.

---
+++ PcPartHelpenator/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PcPartHelpenator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /*
+     * 
+     * ------ GITHUB: https://github.com/B1ADEEE/B1ADEEEs_PC_HELPENATOR
+     * 
+     */
+    public partial class MainWindow : Window
+    {
+        PcHelpenatorEntities db;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            db = new PcHelpenatorEntities();
+            var query = from a in db.CPUs
+                        select a.CPUName;
+            CPU_DROP.ItemsSource = query.ToList();
+        }
+    }
+}
