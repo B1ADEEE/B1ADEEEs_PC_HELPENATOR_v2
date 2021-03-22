1414c556179b1d858ab0e218afb9e7b5a9531144
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2021-03-22 19:33:30 +0000

    Just setting up the project to get ready for a demo and also trying to figure out how to access mulitple db at same time and also trying to make it so when one drop down menu has an item clear the others

--- PcPartHelpenator/MainWindow.xaml.cs
+++ PcPartHelpenator/MainWindow.xaml.cs
@@ -26,6 +26,8 @@ namespace PcPartHelpenator
     public partial class MainWindow : Window
     {
         PcHelpenatorEntities db;
+        int Rating;
+        int Price;
         public MainWindow()
         {
             InitializeComponent();
@@ -34,9 +36,56 @@ namespace PcPartHelpenator
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             db = new PcHelpenatorEntities();
-            var query = from a in db.CPUs
+            var query1 = from a in db.CPUs
                         select a.CPUName;
-            CPU_DROP.ItemsSource = query.ToList();
+            CPU_DROP.ItemsSource = query1.ToList();
+
+            var query2 = from a in db.GPUs
+                        select a.GPUName;
+            GPU_DROP.ItemsSource = query2.ToList();
+
+            var query3 = from a in db.MOTHERBOARDs
+                         select a.MOTHERBOARDName;
+            MOBO_DROP.ItemsSource = query3.ToList();
+
+            var query4 = from a in db.HEATSINKs
+                         select a.HEATSINKName;
+            Heatsink_DROP.ItemsSource = query4.ToList();
+
+            var query5 = from a in db.RAMs
+                         select a.RAMSize;
+            RAM_DROP.ItemsSource = query5.ToList();
+
+            var query6 = from a in db.STORAGEs
+                         select a.STORAGESize;
+            Storage1_DROP.ItemsSource = query6.ToList();
+            Storage2_DROP.ItemsSource = query6.ToList();
+            Storage3_DROP.ItemsSource = query6.ToList();
+
+            var query7 = from a in db.POWERSUPPLies
+                         select a.POWERSUPPLYBrand;
+            PSU_DROP.ItemsSource = query7.ToList();
+
+            var query8 = from a in db.Cases
+                         select a.CASEName;
+
+            if (CaseStealth_DROP.IsDropDownOpen)
+            {
+                CaseStealth_DROP.ItemsSource = query8.ToList();
+            }
+            if (CaseRGB_DROP.IsDropDownOpen)
+            {
+                CaseRGB_DROP.ItemsSource = query8.ToList();
+            }
+            if (Airflow_DROP.IsDropDownOpen)
+            {
+                Airflow_DROP.ItemsSource = query8.ToList();
+            }
+
+            var query9 = from a in db.OPTIONALs
+                         select a.OPTIONALType;
+            OptionalFans_DROP.ItemsSource = query9.ToList();
+            OptionalLED_DROP.ItemsSource = query9.ToList(); 
         }
     }
 }
